d7f0460f0cfbb2599fd8169bac8508061bb64f02
Denis Gabbasov <gabbas.denn@mail.ru>
2023-06-05 18:10:00 +0300

    feat:сверстать header и адаптировать

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+// import { Routes, Route, Link } from 'react-router-dom';
+
+function Header() {
+
+  return (
+    <header className="header page__header">
+        <div className="header__logo"/>
+        <div className="header__movies-links-wrapper">
+          <a href="#" className="header__movies-link">Фильмы</a>
+          <a href="#" className="header__movies-link">Сохранённые фильмы</a>
+        </div>
+        <button className="header__profile-edit-btn">Аккаунт</button>
+        {/* <div className="header__wrapper-auth">
+          <button className="header__register-btn">Регистрация</button>
+          <button className="header__login-btn">Войти</button>
+        </div> */}
+        <button
+          className="header__hamburger"
+          type="button"
+        />
+    </header>
+  );
+}
+
+export default Header;

4f598cff3b8d27939a07aab46e65bbe81bac7153
Denis Gabbasov <gabbas.denn@mail.ru>
2023-06-08 11:08:58 +0300

    feat:сверстать и адаптировать секцию about-me

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,28 @@
+import './AboutMe.css';
+
+import photo1 from '../../images/jevKMA9UEQ0y9fBgFSX8-KnWwQaIAVw84YkxzcqMx4ZMN5aelaZmnsFRbxYnIgMrQe4Qvqa4nCD9R4R63aP6dEE0.jpg'
+import photo2 from '../../images/photo_2023-06-07_17-17-27.jpg'
+import photo3 from '../../images/Изображение WhatsApp 2023-01-19 в 13.25.32.jpg'
+
+function AboutMe() {
+  return (
+    <section className="about-me app__about-me">
+      <h2 className="about-me__title section-title">Студент</h2>
+
+      <div className="about-me__info-container">
+        <div className="about-me__info-text">
+          <h3 className="about-me__name">Денис</h3>
+          <p className="about-me__post-and-age">Фронтенд-разработчик, 22 года</p>
+          <p className="about-me__info-descr">
+            Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
+            и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.
+          </p>
+          <a href="#" className="about-me__github-link github-link">Github</a>
+        </div>
+        <img src={photo2} alt="" className="about-me__photo" />
+      </div>
+    </section>
+  );
+}
+
+export default AboutMe;
--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -3,7 +3,7 @@ import './Techs.css';
 function Techs() {
   return (
     <section className="techs app__techs">
-      <h1 className="section-title">Технологии</h1>
+      <h2 className="techs__title section-title">Технологии</h2>
       <h3 className="techs__subtitle">7 технологий</h3>
       <p className="techs__descr">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
       <ul className="techs__tools">

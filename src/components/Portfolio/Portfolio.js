20391014f2837decda80143006b51101d493b664
Denis Gabbasov <gabbas.denn@mail.ru>
2023-06-08 14:37:27 +0300

    feat:сверстать и адаптировать секцию portfolio

---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,33 @@
+import './Portfolio.css';
+
+import linkImg from '../../images/text__COLOR_font-main.svg'
+
+function Portfolio() {
+  return (
+    <section className="portfolio app__portfolio">
+      <h2 className="portfolio__title">Портфолио</h2>
+      <ul className="portfolio__projects">
+        <li className="porfolio__projects-item">
+          <a href="#" className="porfolio__projects-item-link">
+            <p className="porfolio__projects-item-descr">Статичный сайт</p>
+            <img className="porfolio__projects-item-icon"src={linkImg} alt="" />
+          </a>
+        </li>
+        <li className="porfolio__projects-item">
+          <a href="#" className="porfolio__projects-item-link">
+            <p className="porfolio__projects-item-descr">Адаптивный сайт</p>
+            <img className="porfolio__projects-item-icon"src={linkImg} alt="" />
+          </a>
+        </li>
+        <li className="porfolio__projects-item">
+          <a href="#" className="porfolio__projects-item-link">
+            <p className="porfolio__projects-item-descr">Одностраничное приложение</p>
+            <img className="porfolio__projects-item-icon"src={linkImg} alt="" />
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default Portfolio;
